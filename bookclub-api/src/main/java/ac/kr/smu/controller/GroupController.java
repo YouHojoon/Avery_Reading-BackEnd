2b2c6eebd7952a2d255beebeedecdb8561694c63
hj <dbghwns66@daum.net>
2020-04-28 01:38:35 +0900

    fix:group register

--- bookclub-api/src/main/java/ac/kr/smu/controller/GroupController.java
+++ bookclub-api/src/main/java/ac/kr/smu/controller/GroupController.java
@@ -7,10 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,10 +20,10 @@ public class GroupController {
     private final UserService userService;
 
     @PostMapping
+    @Transactional
     public ResponseEntity<?> postGroup(@RequestBody Group group){
         groupService.register(group);
         userService.setGroup(group);
-        return new ResponseEntity<>("{}", HttpStatus.OK);
+        return new ResponseEntity<>("{}", HttpStatus.CREATED);
     }
-
 }
